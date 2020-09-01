3737fc5ea96a9d0b6e768d282ceb51d372baa100
saifffff <angryn1nj4@gmail.com>
2020-09-01 04:48:26 -0700

    Updates

---
+++ precache-manifest.6d789b2d6f548d71643ed2e216ab9297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29696375a77d0bfbc1506520277c3e7e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "47a984afc53c3d672d29",
+    "url": "/todo/static/css/main.720c5485.chunk.css"
+  },
+  {
+    "revision": "71d9491a072c859f3bd5",
+    "url": "/todo/static/js/2.a4871a49.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.a4871a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a984afc53c3d672d29",
+    "url": "/todo/static/js/main.815620d4.chunk.js"
+  },
+  {
+    "revision": "efd4335b0bc632b6bfc0",
+    "url": "/todo/static/js/runtime-main.5076ecb7.js"
+  }
+]);
